feae0ac17e5aadfeeb5c8be397775c1119f0d48a
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-12-12 11:43:22 +0200

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---front-of-unit-and-parking",
+      "name": "01 - Front of Unit and Parking",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.10069028407152558,
+        "pitch": -0.048874929321387484,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7862095645972023,
+          "pitch": 0.20108673550265976,
+          "rotation": 0,
+          "target": "1-02---open-plan-office"
+        },
+        {
+          "yaw": -1.04800505658379,
+          "pitch": 0.07691702256517452,
+          "rotation": 0,
+          "target": "5-06---street-view-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---open-plan-office",
+      "name": "02 - Open Plan Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9798101087663795,
+          "pitch": 0.16896240230866155,
+          "rotation": 0,
+          "target": "0-01---front-of-unit-and-parking"
+        },
+        {
+          "yaw": -1.7151755884251187,
+          "pitch": 0.25669588805019217,
+          "rotation": 0,
+          "target": "2-03---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---passage",
+      "name": "03 - Passage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15401193193937246,
+          "pitch": 0.5367843422946361,
+          "rotation": 0,
+          "target": "3-04---bathroom"
+        },
+        {
+          "yaw": 1.5350535318908953,
+          "pitch": 0.30861166137679064,
+          "rotation": 0,
+          "target": "1-02---open-plan-office"
+        },
+        {
+          "yaw": -2.8045722758073843,
+          "pitch": 0.7764876946873898,
+          "rotation": 7.0685834705770345,
+          "target": "4-05---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---bathroom",
+      "name": "04 - Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.558007930510737,
+        "pitch": 0.024394865170982882,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8470603394031908,
+          "pitch": 0.6401904698351046,
+          "rotation": 5.497787143782138,
+          "target": "2-03---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---kitchen",
+      "name": "05 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.8061623804376215,
+        "pitch": 0.060458321724883746,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5089197116935598,
+          "pitch": 0.5562981200425838,
+          "rotation": 4.71238898038469,
+          "target": "2-03---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---street-view-of-property",
+      "name": "06 - Street View of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.0216888928982257,
+        "pitch": 0.005639270480301661,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38541840185556353,
+          "pitch": 0.16232387684563676,
+          "rotation": 0,
+          "target": "0-01---front-of-unit-and-parking"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Unit 02, 87 Heugh Road, Walmer",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
